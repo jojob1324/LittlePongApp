4347df96be6d8882a545a655788fb6730f0a1c65
Julius Herz <julius@st-herz.de>
2021-03-16 10:10:05 +0100

    added blue game ball

--- LittlePongApp/GameViewController.swift
+++ LittlePongApp/GameViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 import SpriteKit
-import GameplayKit
+
 
 class GameViewController: UIViewController {
 
@@ -16,44 +16,30 @@ class GameViewController: UIViewController {
         
         // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
         // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
+        let scene = GameScene (size: view.bounds.size)
             
             // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
+         
                 
                 // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
+             
                 
                 // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
+                scene.scaleMode = .aspectFill
                 
                 // Present the scene
                 if let view = self.view as! SKView? {
-                    view.presentScene(sceneNode)
+                    view.presentScene(scene)
                     
                     view.ignoresSiblingOrder = true
                     
                     view.showsFPS = true
                     view.showsNodeCount = true
+                    view.showsPhysics = true;
                 }
             }
         }
-    }
+    
 
-    override var shouldAutorotate: Bool {
-        return true
-    }
 
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
 
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
-}
