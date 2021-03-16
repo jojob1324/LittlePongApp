dca557688dad73b9207cb956d1d5c972fe2c0661
Julius Herz <julius@st-herz.de>
2021-03-16 10:26:44 +0100

    added physical properties to the game ball (frame of the view is the ground)

--- LittlePongApp/GameScene.swift
+++ LittlePongApp/GameScene.swift
@@ -9,12 +9,14 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    let gameBall = SKSpriteNode(color: UIColor.blue, size: CGSize(width: 100, height: 100))
+    let gameBall = SKSpriteNode(color: UIColor.blue, size: CGSize(width: 30, height: 30))
     
     
     override func didMove (to view: SKView) {
         gameBall.position = CGPoint(x: frame.midX, y: frame.midY)
         addChild(gameBall)
+        gameBall.physicsBody = SKPhysicsBody(circleOfRadius: gameBall.size.width/2)
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         
 
 
